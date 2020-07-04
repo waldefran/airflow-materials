3882808bf4d7bde4cbeaf19307afa17978af1612
Marc LAMBERTI <marclamberti.ml@gmail.com>
2020-07-04 15:13:49 +0200

    fix section  with Hive

--- airflow-section-3/docker/hive/hive-server/startup.sh
+++ airflow-section-3/docker/hive/hive-server/startup.sh
@@ -6,4 +6,4 @@ hdfs dfs -chmod g+w   /tmp
 hdfs dfs -chmod g+w   /user/hive/warehouse
 
 cd ${HIVE_HOME}/bin
-./hiveserver2 --hiveconf hive.server2.enable.doAs=false --hiveconf hive.root.logger=INFO,console
+./hiveserver2 --hiveconf hive.server2.enable.doAs=false --hiveconf hive.root.logger=DEBUG,console
